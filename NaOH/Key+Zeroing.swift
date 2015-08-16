0613bb5fa62b68a5a60cd6aba694c6aae34c45e7
Drew Crawford <drew@sealedabstract.com>
2015-08-16 14:47:49 -0500

    Add support for a ZeroingMemory constructor
    Inevitably we need to import keys from some other place (in the instant
    case, FISA wants to import them from a complex file).

---
+++ NaOH/Key+Zeroing.swift
@@ -0,0 +1,24 @@
+//
+//  Key+Zeroing.swift
+//  NaOH
+//
+//  Created by Drew Crawford on 8/16/15.
+//  Copyright © 2015 DrewCrawfordApps. All rights reserved.
+//  This file is part of NaOH.  It is subject to the license terms in the LICENSE
+//  file found inthe top level of this distribution
+//  No part of NaOH, including this file, may be copied, modified,
+//  propagated, or distributed except according to the terms contained
+//  inthe LICENSE file.
+
+import Foundation
+
+extension Key {
+    /**Creates a key from the existing memory, importing it into the NaOH memory system and zeroing out the source */
+    convenience init (inout zeroingMemory: [UInt8]) {
+        self.init(uninitializedSize: zeroingMemory.count)
+        zeroingMemory.withUnsafeMutableBufferPointer { (inout ptr: UnsafeMutableBufferPointer<UInt8>) -> () in
+            memcpy(addrAsVoid, ptr.baseAddress, ptr.count)
+            sodium_memzero(ptr.baseAddress, ptr.count)
+        }
+    }
+}
--- NaOHTests/KeyTests.swift
+++ NaOHTests/KeyTests.swift
@@ -15,8 +15,15 @@ import XCTest
 @testable import NaOH
 
 class KeyTests : XCTestCase {
-func testKey() {
-    let k = try! Key(password: "My password", salt: "My salt is 32 characters   sjej")
+    func testKey() {
+        let k = try! Key(password: "My password", salt: "My salt is 32 characters   sjej")
         XCTAssert(k.hash == "C2D877E295C0070384F1486F18CE136C72B050EFAB71D2830260F2A062B9E2AC")
     }
+    
+    func testZeroImport() {
+        var k : [UInt8] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
+        let key = Key(zeroingMemory: &k)
+        XCTAssert(k == [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+    }
 }
+
