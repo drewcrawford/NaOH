c04f6554f8ffcfbf87b6d39327556957471ede01
Drew Crawford <drew@sealedabstract.com>
2016-06-30 19:13:21 -0500

    Caroline port for AT toolchain

--- NaOHTests/CarolineXCTest.swift
+++ NaOHTests/CarolineXCTest.swift
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+#if !ATBUILD
 import XCTest
 import CarolineCore
 @testable import NaOHTests
@@ -48,3 +48,4 @@ class CarolineEngineTests: XCTestCase {
         }
     }
 }
+#endif
